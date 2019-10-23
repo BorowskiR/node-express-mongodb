d121b66a641dd6a0e04b362afdc36eecedfd880d
Rafał Borowski <rafall.borowski@gmail.com>
2019-10-23 19:59:40 +0200

    config db

--- routes/config.js
+++ routes/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db: '',
+    db: 'mongodb+srv://admin:MJJnmPUExExJfpux@cluster0-5k0tm.mongodb.net/test?retryWrites=true&w=majority',
     keySession: ['TWOJ KLUCZ'], 
     maxAgeSession: 24 * 60 * 60 * 1000
 }
